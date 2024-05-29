420800c13e32c4ca1d183ea78555845f95a5091d
IamCharlyBrown <al02910748@tecmilenio.mx>
2024-05-28 22:02:31 -0600

    modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.jsx 	new file:   src/components/Proyects/Proyects.jsx 	new file:   src/components/Timer/Timer.jsx 	deleted:    src/data/tasks.js 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <ChakraProvider>
+      <div>App</div>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
