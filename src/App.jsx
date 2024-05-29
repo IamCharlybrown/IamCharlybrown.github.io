87f41623a21589ec2618eccebc906b6c249d9319
IamCharlyBrown <al02910748@tecmilenio.mx>
2024-05-28 22:26:48 -0600

    modified:   src/App.jsx 	new file:   src/components/Timer/PlayButton.jsx 	new file:   src/components/Timer/StopButton.jsx 	modified:   src/components/Timer/Timer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Timer from "./components/Timer/Timer";
 function App() {
   return (
     <ChakraProvider>
+      <header></header>
       <main className="flex items-center justify-center h-screen">
         <div className="w-1/6">
           <Timer></Timer>
         </div>
       </main>
+      <footer></footer>
     </ChakraProvider>
   );
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import PlayButton from "./PlayButton";
+import StopButton from "./StopButton";
 
 function Timer() {
-  const percentage = 66;
+  const percentage = 100;
 
-  return <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+  return (
+    <>
+      <CircularProgressbar value={percentage} text={`${percentage}%`} />
+      <PlayButton></PlayButton>
+      <StopButton></StopButton>
+    </>
+  );
 }
 
 export default Timer;
