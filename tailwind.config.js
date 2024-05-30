9079b173e4a0f8bc85fede3d2c8fc55aeccdac4d
IamCharlyBrown <al02910748@tecmilenio.mx>
2024-05-30 16:05:16 -0600

    Error PlayButton fixed 	new file:   src/components/Timer/PlayButton.jsx 	deleted:    src/components/Timer/RestartButton.jsx 	modified:   src/index.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Azul: "#0267C1",
+        Beige: "#E5D2B0",
+        Amarillo: "#EFA00B",
+        Naranja: "#D65108",
+      },
+    },
   },
   plugins: [],
-}
-
+};
