44cb9e3e3168793f8ba39cfe9064d8e67107f55a
IamCharlyBrown <al02910748@tecmilenio.mx>
2024-05-31 14:04:21 -0600

    Proyect module base finished 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/Proyects/AddProyectButton.jsx 	new file:   src/components/Proyects/ProyectCard.jsx 	new file:   src/components/Proyects/ProyectForm.jsx 	deleted:    src/components/Proyects/Proyects.jsx 	new file:   src/components/Proyects/ProyectsList.jsx 	new file:   src/context/ProyectContext.jsx 	new file:   src/data/proyects.js 	modified:   src/main.jsx

---
+++ src/components/Proyects/ProyectCard.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { ProyectContext } from "../../context/ProyectContext";
+import {
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Button,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+
+function ProyectCard({ proyect }) {
+  const { deleteProyect } = useContext(ProyectContext);
+
+  return (
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      variant="outline"
+      marginRight={{ base: 0, md: 200 }}
+    >
+      <Image
+        paddingTop={5}
+        paddingStart={5}
+        objectFit="cover"
+        maxH={{ base: "100%", sm: "120px" }}
+        maxW={{ base: "100%", sm: "200px" }}
+        src={proyect.img}
+        alt={proyect.title}
+      />
+
+      <Stack>
+        <CardBody>
+          <Heading size="md">{proyect.title}</Heading>
+
+          <Text py="2">-{proyect.platform}</Text>
+          <Button variant="solid" colorScheme="blue" marginEnd={4}>
+            Planificar
+          </Button>
+          <Button colorScheme="red" onClick={() => deleteProyect(proyect.id)}>
+            Eliminar
+          </Button>
+        </CardBody>
+      </Stack>
+    </Card>
+  );
+}
+
+export default ProyectCard;
